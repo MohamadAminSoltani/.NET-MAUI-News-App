b32b9d7e47be84c2d84f1546f9ffb751309595b7
M.A.Soltani <maxmadmas@gmail.com>
2022-09-09 13:34:43 +0430

    code refactored

--- NewsApp/Helpers/FileAccessHelper.cs
+++ NewsApp/Helpers/FileAccessHelper.cs
@@ -4,7 +4,7 @@
     {
         public static string GetLocalPath(string filename)
         {
-            return System.IO.Path.Combine(FileSystem.AppDataDirectory, filename);
+            return Path.Combine(FileSystem.AppDataDirectory, filename);
         }
     }
 }
